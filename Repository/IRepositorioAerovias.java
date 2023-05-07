e13868b74c2cfeff5f4c81e3475d05aa16db7d76
Adriana Serpa <adriana.serpa@edu.pucrs.br>
2023-05-07 02:21:05 -0300

    Criação dos repositorios de Aerovias

--- Repository/IRepositorioAerovias.java
+++ Repository/IRepositorioAerovias.java
@@ -1,5 +1,11 @@
 package Repository;
 
-public interface IRepositorioAerovias {
+import Dominio.Aerovias;
+import java.util.List;
 
+public interface IRepositorioAerovias {
+    List <Aerovias>todas();
+    Aerovias getById(String id);
+    Aerovias getByOrigem(String origem);
+    Aerovias getByDestino(String destino);
 }

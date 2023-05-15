ef39b662bc3e566b6ad787893358a63440d19920
pedroFonseca447 <pedrofonseca447@gmail.com>
2023-05-15 19:56:15 -0300

    ta quase

--- src/main/java/com/projarq/planosDeVoo/Controller/PlanoDeVooController.java
+++ src/main/java/com/projarq/planosDeVoo/Controller/PlanoDeVooController.java
@@ -4,6 +4,8 @@ import com.projarq.planosDeVoo.Dominio.AltitudeSlots;
 import com.projarq.planosDeVoo.Dominio.PlanoDeVoo;
 import com.projarq.planosDeVoo.Service.ServicoPlano;
 
+import java.lang.reflect.Array;
+import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -24,7 +26,6 @@ public class PlanoDeVooController {
         return servicoPlano.cancelarPlano(idplano);
     }
 
-
     @PostMapping(value = "/scta/slotslivres", consumes = "application/json", produces = "application/json")
     public AltitudeSlots slotsLivres(@RequestAttribute(name = "rota") Long idAerovia,
             @RequestAttribute(name = "data") LocalDate data,
@@ -32,4 +33,22 @@ public class PlanoDeVooController {
             @RequestAttribute(name = "idAeronave") Long idAeronave){
         return servicoPlano.getSlotsLivres(idAerovia, LocalDateTime.of(data, horario), idAeronave);
     }
+
+    @PostMapping(value = "/scta/aprovaPlanoDeVoo", consumes = "application/json", produces = "application/json")
+    public String aprovAltitudeSlots(@RequestAttribute(name = "rota") Long idAerovia,
+            @RequestAttribute(name = "id") BigInteger id,
+            @RequestAttribute(name = "altitude") Integer altitude,
+            @RequestAttribute(name = "cancelado") Boolean cancelado,
+            @RequestAttribute(name = "data_horario_inicio") LocalTime inicio,
+            @RequestAttribute(name = "data_horario_fim") LocalTime fim,
+            @RequestAttribute(name = "id_aeronave") BigInteger id_aeronave,
+            @RequestAttribute(name = "id_aerovia") BigInteger id_aerovia,
+            @RequestAttribute(name = "matric_piloto") BigInteger matric_piloto,
+            @RequestAttribute(name = "slots") Integer[] slots){
+                
+                String zeDaManga = "";
+        return zeDaManga;
+    }
+
+
 }
